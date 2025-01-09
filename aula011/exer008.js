4d561ca18451ed3da22affae4a851dbd75e8808d
Danilo224 <adonilio.silva.pereira@gmail.com>
2025-01-09 22:24:46 +0000

    exer010

---
+++ aula011/exer008.js
@@ -0,0 +1,7 @@
+var vel = 70
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60 )  { // condição simples !
+    console.log(`Você ultrapassou a velocidade permitido. Multado!`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
+        
