376d7f045a3823066add02a99ff1b4d40ed2cd47
Danilo224 <adonilio.silva.pereira@gmail.com>
2025-01-14 11:47:53 +0000

    aula015

---
+++ aula015/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9, 3]
+   num.sort()
+   num.push(1)
+   console.log(num)
+   console.log(`o vetor tem ${num.length} posições.`)
+   console.log(`o primeiro vetor e ${num[0]}`)
+   let pos = num.indexOf(9)
+   console.log(`o valor 9 esta na posição ${pos}`)
+
