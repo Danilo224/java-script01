3e8858e543ed4ef2be309cb0f73249aed6b9ba90
Danilo224 <adonilio.silva.pereira@gmail.com>
2025-01-15 18:10:50 +0000

    aula 016

--- aula016/script.js
+++ aula016/script.js
@@ -1,1 +1,25 @@
-function adicionar()
+let num = document.querySelector("input#fnum") 
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+function isNumero(n) {
+     if(Number(n) >=1 && Number(n) <= 100) {
+         return true
+     } else {
+         return false
+     }
+   }
+function inlista(n, l) {
+      if (l.indexOf(Number(n)) != -1) {
+         return true
+      } else {
+         return false
+      }
+   }
+function adicionar() {
+   if(isNumero(num.value) && !inlista(num.value, valores)) {
+      alert('Tudo ok')
+   } else {
+      alert('Valor invalido ou ja encontrado na lista')
+   }
+}
